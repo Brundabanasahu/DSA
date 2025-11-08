641ab476440c8b19b6801cdec1a2ea863cb61f56
Brundabana Sahu <sbrundabana12@gmail.com>
2025-11-08 10:42:36 +0530

    Commit

--- Patterns/LeftAlignedInverseNumber.java
+++ Patterns/LeftAlignedInverseNumber.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
-public class LeftAlignedNumber {
+public class LeftAlignedInverseNumber {
+    
     public static void main(String[] args) {
-        
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the number: ");
-        int n=sc.nextInt();;
-        for(int i=0;i<=n;i++)
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
         {
-            for(int j=1;j<=i;j++)
+            for(int j=1;j<=n-i+1;j++)
             {
                 System.out.print(j);
             }
--- Patterns/LeftAlignedNumber2.java
+++ Patterns/LeftAlignedNumber2.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
-public class LeftAlignedNumber {
+public class LeftAlignedNumber2 {
     public static void main(String[] args) {
-        
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the number: ");
-        int n=sc.nextInt();;
-        for(int i=0;i<=n;i++)
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=i;j++)
             {
-                System.out.print(j);
+                System.out.print(i);
+    
             }
-            System.out.println();
+            System.out.println( );
         }
     }
 }
