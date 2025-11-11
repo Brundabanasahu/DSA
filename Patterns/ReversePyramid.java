964d6428ab3002f82c72e5a4d39a9dcb903d9314
Brundabana Sahu <sbrundabana12@gmail.com>
2025-11-11 19:53:53 +0530

    Pyramid Problem

---
+++ Patterns/ReversePyramid.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ReversePyramid {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+
+            //Space
+            for(int j=0;j<i;j++)
+            {
+                System.out.print(" ");
+            }
+
+            //Star
+            for(int j=0;j<2*n-(2*i-1);j++)
+            {
+            System.out.print("*");
+            }
+
+            //Space
+            for(int j=0;j<i;j++)
+            {
+                System.out.print(" ");
+            }
+            System.out.println();
+
+        }
+    }
+}
