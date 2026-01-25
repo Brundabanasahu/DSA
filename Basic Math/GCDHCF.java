e774ddb2c4282daeb64a6ba47a91841b90bb1a16
Brundabana Sahu <sbrundabana12@gmail.com>
2026-01-25 18:46:46 +0530

    Basic Math is over

--- Basic Math/GCDHCF.java
+++ Basic Math/GCDHCF.java
@@ -7,6 +7,22 @@ public class GCDHCF {
         int num1 = scanner.nextInt();
         System.out.print("Enter second number: ");
         int num2 = scanner.nextInt();
+        while(num1>0 && num2>0){
+            if(num1>num2){
+                num1=num1%num2;
+            }
+            else{
+                num2=num2%num1;
+            }
+            if(num1==0){
+                System.out.println("GCD is: "+num2);
+            }
+            else if(num2==0){
+                System.out.println("GCD is: "+num1);
+            }
+        }
     }
     
 }
+//i solve this problem using ecuclidean algorithm
+//their formula or logic is : GCD(a,b)=GCD(a-b,b) keep doing the apply ecucliean algorithm untill one of them is zero.
