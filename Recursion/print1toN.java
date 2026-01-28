1929f10d212c91d1ee150358f3928db44dfa7e22
Brundabana Sahu <sbrundabana12@gmail.com>
2026-01-28 23:01:17 +0530

    Name n times

---
+++ Recursion/print1toN.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class print1toN {
+    public static void num(int n)
+    {
+        if (n == 0) {
+            return;
+        }
+        num(n - 1);
+        System.out.println(n);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();   
+        num(n);
+    }
+}
